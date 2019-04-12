9cd91b9a25053fe478eebf5793c59f28b4a5c96f
Robert Curl <curl.robert@gmail.com>
2019-04-12 16:25:49 -0400

    Update init.js

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -219,6 +219,9 @@ module.exports = function(
 
   console.log();
   console.log(`Success! Created ${appName} at ${appPath}`);
+  console.log();
+  console.log(chalk.purple(`Hello, ${process.env.USERNAME || 'USER'}`));
+  console.log();
   console.log('Inside that directory, you can run several commands:');
   console.log();
   console.log(chalk.cyan(`  ${displayedCommand} start`));
